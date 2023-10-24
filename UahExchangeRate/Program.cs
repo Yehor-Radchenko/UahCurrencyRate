5e152fc7fba425ac2ca0bfaf6ff3d453ee9a7038
Yehor-Radchenko <115692958+Yehor-Radchenko@users.noreply.github.com>
2023-10-24 19:10:39 +0300

    Add project files.

---
+++ UahExchangeRate/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
